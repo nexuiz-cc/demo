3ce3ba500670fc1008a81bd78fa0de312255c3f7
nexuiz-cc <41991563+nexuiz-cc@users.noreply.github.com>
2024-03-23 14:10:05 +0900

    ...

--- src/router/RouterPages.tsx
+++ src/router/RouterPages.tsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import HomePage from '../pages/HomePage';
 import ListUser from '../pages/user/ListUser';
 import NewUser from '../pages/NewUser';
+import Login from '../pages/login/Login';
 
 function RouterPages() {
   return (
@@ -10,6 +11,7 @@ function RouterPages() {
       <Route path="/" element={<HomePage />} />
       <Route path='/user' element={<ListUser />} />
       <Route path='/newUser' element={<NewUser />} />
+      <Route path='/login' element={<Login />} />
     </Routes>
   );
 }
