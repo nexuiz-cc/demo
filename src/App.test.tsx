80bf8980add33e66a92269b885bc8717e1da00b0
nexuiz-cc <41991563+nexuiz-cc@users.noreply.github.com>
2024-03-23 13:48:11 +0900

    ..

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import ToggleColorMode from './App';
+import { BrowserRouter } from 'react-router-dom';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+describe("App routing", () => {
+  test("default root", () => {
+    render(<ToggleColorMode />, { wrapper: BrowserRouter });
+    // verify page content for default route
+    expect(screen.getByText(/My APP/i)).toBeInTheDocument();
+  });
 });
