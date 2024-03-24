9859c5d32a06b410e16e1a902a668e94ac167e97
nexuiz-cc <41991563+nexuiz-cc@users.noreply.github.com>
2024-03-24 14:04:30 +0900

    ...

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -15,33 +15,49 @@ import './login.scss';
 function Login() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-  const [message, setMessage] = useState('');
+  const [message1, setMessage1] = useState('');
+  const [message2, setMessage2] = useState('');
+  const [sever,setSeverity]= useState('error');
   const navgate = useNavigate();
   const [inputError, setInputError] = useState(false);
   const [msg1, setMsg1] = useState('');
   const [msg2, setMsg2] = useState('');
-  const [open, setOpen] = useState(false);
+  const [open1, setOpen1] = useState(false);
+  const [open2, setOpen2] = useState(false);
 
-  const handleClose = (event?: React.SyntheticEvent | Event, reason?: string) => {
-    if (reason === 'clickaway') { return;} setOpen(false)
+  const handleClose1 = (event?: React.SyntheticEvent | Event, reason?: string) => {
+    if (reason === 'clickaway') { return; } setOpen1(false)
   };
-   
+  const handleClose2 = (event?: React.SyntheticEvent | Event, reason?: string) => {
+    if (reason === 'clickaway') { return; } setOpen2(false)
+  };
+
   const handleLogin = () => {
     login().then((res) => {
       if (username === res.data[0].username && password === res.data[0].password) {
-        setMessage('login success');
-        setOpen(true);
+        setMessage1('login success');
+        setOpen1(true);
         let token = "token123";
         localStorage.setItem('userToken', token);
+        navgate(0);
       } else {
-        if (username !== res.data[0].username) {
+        if (username === "") {
           setInputError(true);
           setMsg1('Please Enter username.');
         }
-        if (password !== res.data[0].password) {
+        if (password === "") {
           setInputError(true);
           setMsg2('Please Enter password.');
         }
+        if (username !== "" && username !== res.data[0].username) {
+          setMessage1('You Iput Useranme is incorrect.')
+          setOpen1(true);
+        }
+        if (password !== "" && password !== res.data[0].password) {
+          setMessage2('You Iput password is incorrect.')
+          setOpen2(true);
+        }
+
         let token = "logout";
         localStorage.setItem('userToken', token);
       }
@@ -87,9 +103,14 @@ function Login() {
       <Card component="form" className={styles.card} noValidate autoComplete="off">
         {card}
       </Card>
-      <Snackbar anchorOrigin={{ vertical: 'top', horizontal: 'center' }} open={open} >
-        <Alert onClose={handleClose} variant="filled" severity="error" className={styles.alert}>
-          {message}
+      <Snackbar anchorOrigin={{ vertical: 'top', horizontal: 'center' }} open={open1} >
+        <Alert onClose={handleClose1} variant="filled" severity="error" className={styles.alert1}>
+          {message1}
+        </Alert>
+      </Snackbar>
+      <Snackbar anchorOrigin={{ vertical: 'top', horizontal: 'center' }} open={open2} >
+        <Alert onClose={handleClose2} variant="filled" severity="error" className={styles.alert2}>
+          {message2}
         </Alert>
       </Snackbar>
     </div>
