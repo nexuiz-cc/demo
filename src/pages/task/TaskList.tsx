bb1ca88767dad8d0af6c018d928bc17ebd0b610b
nexuiz-cc <hyd524132502@gmail.com>
2024-07-11 19:39:00 +0900

    ...

--- src/pages/task/TaskList.tsx
+++ src/pages/task/TaskList.tsx
@@ -1,37 +1,161 @@
+import "./styles.css";
+import {
+  closestCorners,
+  DndContext,
+  KeyboardSensor,
+  PointerSensor,
+  useSensor,
+  useSensors,
+  DragEndEvent,
+  DragOverEvent
+} from "@dnd-kit/core";
+import { arrayMove, sortableKeyboardCoordinates } from "@dnd-kit/sortable";
+import Column, { ColumnType } from "./Column";
+import { useState } from "react";
 
-import React from 'react';
-import Grid from '@mui/material/Grid';
-import './task.css';
+export default function TaskList() {
+  // 仮データを定義
+  const data: ColumnType[] = [
+    {
+      id: "Column1",
+      title: "TODO",
+      cards: [
+        {
+          id: "Card1",
+          title: "Card1",
+          content:'card1 content'
+        },
+        {
+          id: "Card2",
+          title: "Card2",
+          content:'card2 content'
+        }
+      ]
+    },
+    {
+      id: "Column2",
+      title: "DOING",
+      cards: [
+        
+      ]
+    },{
+      id: "Column3",
+      title: "DONE",
+      cards: [
+        
+      ]
+    }
+  ];
+  const [columns, setColumns] = useState<ColumnType[]>(data);
 
+  const findColumn = (unique: string | null) => {
+    if (!unique) {
+      return null;
+    }
+    // overの対象がcolumnの場合があるためそのままidを返す
+    if (columns.some((c) => c.id === unique)) {
+      return columns.find((c) => c.id === unique) ?? null;
+    }
+    const id = String(unique);
+    const itemWithColumnId = columns.flatMap((c) => {
+      const columnId = c.id;
+      return c.cards.map((i) => ({ itemId: i.id, columnId: columnId }));
+    });
+    const columnId = itemWithColumnId.find((i) => i.itemId === id)?.columnId;
+    return columns.find((c) => c.id === columnId) ?? null;
+  };
 
+  const handleDragOver = (event: DragOverEvent) => {
+    const { active, over, delta } = event;
+    const activeId = String(active.id);
+    const overId = over ? String(over.id) : null;
+    const activeColumn = findColumn(activeId);
+    const overColumn = findColumn(overId);
+    if (!activeColumn || !overColumn || activeColumn === overColumn) {
+      return null;
+    }
+    setColumns((prevState) => {
+      const activeItems = activeColumn.cards;
+      const overItems = overColumn.cards;
+      const activeIndex = activeItems.findIndex((i) => i.id === activeId);
+      const overIndex = overItems.findIndex((i) => i.id === overId);
+      const newIndex = () => {
+        const putOnBelowLastItem =
+          overIndex === overItems.length - 1 && delta.y > 0;
+        const modifier = putOnBelowLastItem ? 1 : 0;
+        return overIndex >= 0 ? overIndex + modifier : overItems.length + 1;
+      };
+      return prevState.map((c) => {
+        if (c.id === activeColumn.id) {
+          c.cards = activeItems.filter((i) => i.id !== activeId);
+          return c;
+        } else if (c.id === overColumn.id) {
+          c.cards = [
+            ...overItems.slice(0, newIndex()),
+            activeItems[activeIndex],
+            ...overItems.slice(newIndex(), overItems.length)
+          ];
+          return c;
+        } else {
+          return c;
+        }
+      });
+    });
+  };
 
-function TaskList() {
+  const handleDragEnd = (event: DragEndEvent) => {
+    const { active, over } = event;
+    const activeId = String(active.id);
+    const overId = over ? String(over.id) : null;
+    const activeColumn = findColumn(activeId);
+    const overColumn = findColumn(overId);
+    if (!activeColumn || !overColumn || activeColumn !== overColumn) {
+      return null;
+    }
+    const activeIndex = activeColumn.cards.findIndex((i) => i.id === activeId);
+    const overIndex = overColumn.cards.findIndex((i) => i.id === overId);
+    if (activeIndex !== overIndex) {
+      setColumns((prevState) => {
+        return prevState.map((column) => {
+          if (column.id === activeColumn.id) {
+            column.cards = arrayMove(overColumn.cards, activeIndex, overIndex);
+            return column;
+          } else {
+            return column;
+          }
+        });
+      });
+    }
+  };
+
+  const sensors = useSensors(
+    useSensor(PointerSensor),
+    useSensor(KeyboardSensor, {
+      coordinateGetter: sortableKeyboardCoordinates
+    })
+  );
   return (
-    <div>
-      MyTaskList
-
-      <Grid container sx={{display:'flex',alignContent:'center',justifyContent:'center'}}>
-        <Grid item xs={2.2} className='item'>
-          item
-        </Grid>
-        <Grid item xs={2.2} className='item'>
-          item
-        </Grid>
-        <Grid item xs={2.5} className='item'>
-          item
-        </Grid>
-        <Grid item xs={2.2} className='item'>
-          item
-        </Grid>
-        <Grid item xs={2.2} className='item'>
-          item
-        </Grid>
-
-      </Grid>
-
-
-    </div>
+    // 今回は長くなってしまうためsensors、collisionDetectionなどに関しての説明は省きます。
+    // ひとまずは一番使われていそうなものを置いています。
+    <DndContext
+      sensors={sensors}
+      collisionDetection={closestCorners}
+      onDragEnd={handleDragEnd}
+      onDragOver={handleDragOver}
+    >
+      <div
+        className="App"
+        style={{ display: "flex", flexDirection: "row", padding: "20px" }}
+      >
+        {columns.map((column) => (
+          <Column
+            key={column.id}
+            id={column.id}
+            title={column.title}
+            cards={column.cards}
+          ></Column>
+        ))}
+      </div>
+    </DndContext>
   );
 }
-
-export default TaskList;
